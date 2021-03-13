4f9508fe13f16c1e2959eed19592e7965e6ee999
Can Ural <can.ural@outlook.com>
2021-03-13 16:27:16 +0200

    Created team and mentors pages. Created a reusable card component to use it in both team and mentors pages. Fixed footer position in different pages. Changed favicon to the new logo. Changed header image with an inspirational image from unsplash. Changed padding for the card under tl;dr. Changed links from a to next/link. For now, keeping the placeholders down until changing the introduction with putting the placeholder under it maybe.

--- components/layout.js
+++ components/layout.js
@@ -3,10 +3,10 @@ import Footer from '../components/footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="bg-gray-50">
-      <div className="container mx-auto text-xl sm:text-2xl px-2 sm:px-4 md:px-6 text-gray-800 tracking-tighter">
+    <div className="bg-gray-50 ">
+      <div className="container flex flex-col h-screen max-w-screen-lg mx-auto text-xl sm:text-2xl px-2 sm:px-4 md:px-6 text-gray-800 tracking-tighter">
         <Header />
-        {children}
+        <main className="flex-grow">{children}</main>
         <Footer />
       </div>
     </div>
---
+++ pages/team.js
@@ -0,0 +1,35 @@
+import Card from '../components/card';
+import Layout from '../components/layout';
+
+const teamMembers = [
+  {
+    name: 'Eduardo Vedes',
+    location: 'PT',
+    image: '/assets/team/eduardo.jpg',
+    title: 'Software Developer',
+    description: 'Self-taught developer focusing on React, Vue and Node',
+    twitter: 'eduardovedes',
+    linkedIn: 'evedes'
+  },
+  {
+    name: 'Can Ural',
+    location: 'FI',
+    image: '/assets/team/can.jpg',
+    title: 'Full Stack Developer',
+    description: 'Self-taught full stack developer, focusing on React/JS',
+    twitter: 'urlDev',
+    linkedIn: 'can-ural'
+  }
+];
+
+export default function Team() {
+  return (
+    <Layout>
+      <div className="md:grid grid-cols-2 gap-4 mt-8">
+        {teamMembers.map((team) => (
+          <Card team={team} key={team.name} />
+        ))}
+      </div>
+    </Layout>
+  );
+}
