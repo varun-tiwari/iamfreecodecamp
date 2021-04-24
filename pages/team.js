7d177ae7a0607579b22cef318cd65b4c50276ad6
Dan Bloch <danjobloch@gmail.com>
2021-04-24 17:41:27 +0100

    Added entry to team page

--- pages/team.js
+++ pages/team.js
@@ -106,6 +106,49 @@ export default function Team() {
             </h1>
           </div>
         </div>
+        <div className="md:flex border-solid border-2 border-gray-200  rounded-md shadow-lg mt-8">
+          <div className="bg-gray-100 p-8 w-full md:w-5/12 lg:w-2/5 xl:w-1/4 ">
+            <img
+              src="/assets/team/dan.jpg"
+              alt=""
+              className="w-40 h-40 sm:w-56 sm:h-56 rounded-full mx-auto"
+            />
+            <h1 className="text-blue-400 text-lg md:text-2xl flex justify-center mt-4">
+              Dan Bloch
+            </h1>
+            <div className="flex justify-center mt-4 ">
+              <a
+                href={`https://twitter.com/bookim_danno`}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="my-auto">
+                <FaTwitter className="w-6 h-6 md:w-8 md:h-8 hover:text-twitter-200 text-twitter-100 transition duration-300 ease-in-out" />
+              </a>
+              <a
+                href={`https://www.linkedin.com/in/dan-bloch/`}
+                target="_blank"
+                rel="noreferrer noopener">
+                <FaLinkedin className="w-6 h-6 md:w-8 md:h-8 ml-4  hover:text-linkedIn-200 text-linkedIn-100 transition duration-300 ease-in-out" />
+              </a>
+            </div>
+          </div>
+
+          <div className="w-full md:w-7/12 lg:w-3/5 xl:w-3/4  px-4 py-8 sm:p-12">
+            <h1>
+              Hello, I&apos;m Dan, a front-end developer-in-waiting who&apos;s also 
+              arrived here after changing careers. <br /><br /> 
+              After 23 years as a reporter and in production for business magazines, 
+              I took voluntary redundancy in September 2020 and I&apos;ve been learning 
+              to code ever since. The past year has highlighted more than ever
+              the inevitability of change &ndash; and how it&apos;s vital we embrace it.<br /><br />
+              I&apos;m interested in languages, people, travel, history, music, film and politics.
+              Don&apos;t know what the collective noun for owls is? Look it up! 
+
+        
+            
+            </h1>
+          </div>
+        </div>
       </div>
     </Layout>
   );
